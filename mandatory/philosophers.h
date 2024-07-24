a09a864423d83e8dae07ee9ead20f0e51a84925b
maria <mariafortunatom@gmail.com>
2024-07-24 14:11:58 +0100

    structs created

--- mandatory/philosophers.h
+++ mandatory/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: mfortuna <mfortuna@student.42.pt>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 11:20:23 by mfortuna          #+#    #+#             */
-/*   Updated: 2024/07/23 14:45:07 by mfortuna         ###   ########.fr       */
+/*   Updated: 2024/07/24 12:15:16 by mfortuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,38 @@
 # include <sys/time.h>
 # include <pthread.h>
 # include <errno.h>
+# include <stdbool.h>
 
-typedef struct s_phil
+typedef struct s_data
 {
 	int			n_phil;
 	int			t_die;
 	int			t_eat;
 	int			t_sleep;
 	int			x_eat;
-}				t_phil
+}				t_data;
+
+typedef struct s_phil
+{
+	struct s_phil		*prev;
+	struct s_phil		*next;
+	t_data				data;
+	int					id;
+	bool				left_fork;
+	bool				rigth_fork;
+	bool				eat;
+	bool				sleep;
+	bool				think;
+}						t_phil;
+
+int		ft_get_data(t_data *data, int argc, char **argv);
+int		get_nbr(char *s);
+void	create_struct(t_phil **head, t_data data);
+void	add_node(t_phil **head, int nbr, t_data data);
+t_phil	*create_node(int nbr, t_data data);
+void	delete_node(t_phil **head);
+void	delete_stack(t_phil **head);
+
+
 
 #endif
--- mandatory/src/main.c
+++ mandatory/src/main.c
@@ -6,15 +6,57 @@
 /*   By: mfortuna <mfortuna@student.42.pt>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 14:24:42 by mfortuna          #+#    #+#             */
-/*   Updated: 2024/07/23 14:47:26 by mfortuna         ###   ########.fr       */
+/*   Updated: 2024/07/24 13:47:22 by mfortuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
 
-ft_get_data(t_phil *data, int argc, char **argv)
+int	get_nbr(char *s)
 {
-	
+	int		i;
+	long	nbr;
+
+	i = -1;
+	nbr = 0;
+	while (s[++i])
+	{
+		if ((s[i] == '-' || s[i] == '+') && i == 0)
+		{
+			if (s[i] == '-')
+				return (-1);
+		}
+		else if (s[i] < '0' || s[i] > '9')
+			return (-1);
+		else
+			nbr = (nbr * 10) + (s[i] - '0');
+		if (nbr > 2147483647)
+			return (-1);
+	}
+	return (nbr);
+}
+
+int	ft_get_data(t_data *data, int argc, char **argv)
+{
+	data->n_phil = get_nbr(argv[1]);
+	if (data->n_phil < 1)
+		return (-1);
+	data->t_die = get_nbr(argv[2]);
+	if (data->t_die < 0)
+		return (-1);
+	data->t_eat = get_nbr(argv[3]);
+	if (data->t_eat < 0)
+		return (-1);
+	data->t_sleep = get_nbr(argv[4]);
+	if (data->t_sleep < 0)
+		return (-1);
+	if (argc == 6)
+	{
+		data->x_eat = get_nbr(argv[5]);
+		if (data->x_eat < 0)
+			return (-1);
+	}
+	return (0);
 }
 
 // arguments:
@@ -23,13 +65,19 @@ ft_get_data(t_phil *data, int argc, char **argv)
 // time to eat
 // time to sleep
 // optional number of times a philosopher has to eat - if not specified, the program only stops when a philosopher dies
-int	main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_phil	data;
-	
+	t_data	data;
+	t_phil	*head;
+
+	head = NULL;
 	if (argc > 4 && argc < 7)
 	{
-		ft_get_data(&data, argc, argv);
+		if (ft_get_data(&data, argc, argv) < 0)
+			return (printf("wrong input\n"));
+		create_struct(&head, data);
+		delete_stack(&head);
+		return (0);
 	}
 	return (printf("invalid number of arguments\n"));
 }
