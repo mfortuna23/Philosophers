93ef678f8457b5f2ba0be1ed49cad4e9cc523274
Maria Fortunato Moreira <mfortuna@c1r1s3.42porto.com>
2024-08-30 12:03:39 +0100

    done

---
+++ mandatory/philosophers.h
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfortuna <mfortuna@student.42.pt>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 11:26:12 by mfortuna          #+#    #+#             */
+/*   Updated: 2024/08/30 11:31:27 by mfortuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <errno.h>
+# include <stdbool.h>
+
+typedef enum e_op
+{
+	SIM,
+	EAT,
+	DIED,
+}			t_op;
+
+typedef struct s_data
+{
+	long int			n_phil;
+	long int			t_die;
+	long int			t_eat;
+	long int			t_sleep;
+	long int			x_eat;
+	long int			x_eaten;
+	pthread_t			*phil;
+	pthread_mutex_t		w;
+	pthread_mutex_t		eat;
+	pthread_mutex_t		dead;
+	pthread_mutex_t		time;
+	struct timeval		start;
+	struct timeval		now;
+	int					sim;
+	pthread_mutex_t		*forks;
+	struct s_phil		*head;
+}						t_data;
+
+typedef struct s_phil
+{
+	int					id;
+	int					t_eaten;
+	long int			lastmeal;
+	pthread_mutex_t		*r_fork;
+	pthread_mutex_t		*l_fork;
+	struct s_phil		*next;
+	int					dead;
+	int					full;
+	struct s_data		*data;
+}						t_phil;
+
+void	create_struct(t_phil **head, t_data *data);
+void	add_last(t_phil **head, t_data *data, int id);
+t_phil	*create_node(t_data *data, int id);
+void	clean_struct(t_phil **head);
+void	delete_last(t_phil **head);
+int		ft_strcomp(const char *s1, const char *s2);
+int		ft_stop(t_phil *phil);
+#endif
---
+++ mandatory/src/struct.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfortuna <mfortuna@student.42.pt>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 11:25:42 by mfortuna          #+#    #+#             */
+/*   Updated: 2024/08/30 11:31:46 by mfortuna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philosophers.h"
+
+void	assigned_forks(t_phil *node, t_data *data, int id)
+{
+	if (id == 1)
+	{
+		node->r_fork = &data->forks[data->n_phil - 1];
+		node->l_fork = &data->forks[id - 1];
+	}
+	else if (id % 2 == 0)
+	{
+		node->l_fork = &data->forks[id - 2];
+		node->r_fork = &data->forks[id - 1];
+	}
+	else
+	{
+		node->r_fork = &data->forks[id - 2];
+		node->l_fork = &data->forks[id - 1];
+	}
+}
+
+t_phil	*create_node(t_data *data, int id)
+{
+	t_phil	*node;
+
+	node = malloc(sizeof(t_phil));
+	node->id = id;
+	node->t_eaten = 0;
+	node->lastmeal = 0;
+	assigned_forks(node, data, id);
+	node->full = 0;
+	node->data = data;
+	node->dead = 0;
+	node->next = NULL;
+	return (node);
+}
+
+void	add_last(t_phil **head, t_data *data, int id)
+{
+	t_phil	*current;
+
+	if ((*head) == NULL)
+	{
+		(*head) = create_node(data, id);
+		return ;
+	}
+	current = *head;
+	if (current->next == NULL)
+	{
+		current->next = create_node(data, id);
+		return ;
+	}
+	while (current->next != NULL)
+		current = current->next;
+	current->next = create_node(data, id);
+}
+
+void	create_struct(t_phil **head, t_data *data)
+{
+	int		i;
+
+	i = 0;
+	while (i < data->n_phil)
+	{
+		add_last(head, data, i + 1);
+		i++;
+	}
+}
+void	delete_last(t_phil **head)
+{
+	t_phil	*last;
+	t_phil	*del;
+
+	last = (*head);
+	while (last->next->next != NULL)
+		last = last->next;
+	del = last->next;
+	last->next = NULL;
+	free (del);
+	return ;
+}
+
+
+void	clean_struct(t_phil **head)
+{
+	while ((*head)->next != NULL)
+		delete_last(head);
+	free(*head);
+	return ;
+}
